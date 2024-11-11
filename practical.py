2ec99d7661632b8c46e20651b5dd717c9f82e851
Gajji Madhuri <madhuri.madhu1708@gmail.com>
2024-11-11 18:15:47 +0530

    Update practical.py

--- practical.py
+++ practical.py
@@ -1,6 +1,9 @@
 #print a message to the console
    print("hello,world!")
 
+#write a python program to print Hello students!......welcome to the lab
+print("Hello students !......welcome to the lab")
+
 #variable
 a=100
  print(a)
