7767ed16837fac425a3371aa6bbef4b37fde670c
Gajji Madhuri <madhuri.madhu1708@gmail.com>
2024-11-11 18:30:35 +0530

    Update practical.py

--- practical.py
+++ practical.py
@@ -4,6 +4,23 @@
 #write a python program to print Hello students!......welcome to the lab
 print("Hello students !......welcome to the lab")
 
+#write a python program to print
+("Twinkle twinkle little start,
+	 How I wonder what your are 
+up above the world
+	 so high like a diamond in the sky")
+  
+   print("Twinkle twinkle little start,")
+   print("\t How I wonder what your are ")
+   print("up above the world")
+   print("\t so high like a diamond in the sky")
+
+   #write a python program to find area of circle
+r=float(input("input the radius of the circle:"))
+pi=3.14
+area=pi*r*r
+print("the area of circle:",area)
+   
 #variable
 a=100
  print(a)
